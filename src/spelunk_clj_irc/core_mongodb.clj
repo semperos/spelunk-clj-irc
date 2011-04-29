c102c7eff271b17026fd18c004d96bc95e4a31e1
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-29 18:41:02 -0400

    Add support for MongoDB

---
+++ src/spelunk_clj_irc/core_mongodb.clj
@@ -0,0 +1,16 @@
+(in-ns 'spelunk-clj-irc.core)
+
+(def mongodb-coll :logs)
+
+(defn nodes-to-mongodb
+  [_ url nodes]
+  (let [comments (html-to-comments url nodes)]
+    (doseq [comment comments]
+      (mongo/insert! mongodb-coll
+                     (assoc comment :when (.toDate (:when comment)))))))
+
+(comment
+
+  (mongo/mongo! :db "clj_irc")
+
+  )
