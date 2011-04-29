f30b37c45a03010c593dd13ba79d12230ecbe76d
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2011-04-29 14:38:36 -0400

    Add most recent JDBC driver for MySQL

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [enlive "1.0.0"]
-                 [clj-time "0.3.0"]]
+                 [clj-time "0.3.0"]
+                 [mysql/mysql-connector-java "5.1.16"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
